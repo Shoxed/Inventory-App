8bb8620e193ef5086b0d12da96adbd6b02109932
Jonathan Hickey <jhickey2021@gmail.com>
2023-10-25 23:11:30 -0600

    Created Item Model for Food

--- inventory_app/models.py
+++ inventory_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    CATEGORY = [
+        ("Dairy", "Dairy"),
+        ("Protein", "Protein"),
+        ("Bread", "Bread"),
+        ("Fruit", "Fruit"),
+        ("Vegetable", "Vegetable"),
+        ("Beverage", "Beverage"),
+        ]
+    
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=CATEGORY)
+    cost = models.FloatField()
+    amount = models.IntegerField()
+
--- inventory_app/views.py
+++ inventory_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+# Render index.html
+    return render( request, 'inventory_app/index.html')
+
